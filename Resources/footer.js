b6fa142c4bfea93a4f2f35f1655fad300cb64da2
Matt Ricks <mgizmor37@gmail.com>
2017-11-18 10:01:36 -0700

    Changed for hosting on github

---
+++ Resources/footer.js
@@ -0,0 +1,12 @@
+var footerTemplate = `
+<a href="file:///Users/mattricks/voyager-archives/src/about.html">About</a>
+<!-- <a href="file:///Users/mattricks/voyager-archives/src/FAQ.html">FAQ</a> -->
+<a href="file:///Users/mattricks/voyager-archives/src/news.html">News</a>
+<!-- <a href="file:///Users/mattricks/voyager-archives/src/terms.html">Terms</a> -->
+<!-- <a href="file:///Users/mattricks/voyager-archives/src/privacy.html">Privacy</a> -->
+<!-- <a href="file:///Users/mattricks/voyager-archives/src/cookies.html">Cookies</a> -->
+<a href="file:///Users/mattricks/voyager-archives/src/contact.html">Contact</a>
+<div>Portions of this content are copyright 2017 by individual Voyager Archives contributors. Content available under a Creative Commons license.</div>
+<div>Website created and maintained by <a href="https://github.com/Emrix">Matt Ricks</a>.</div>
+<div>Last updated on <%updatedDate%></div>
+`;
---
+++ Resources/header.js
@@ -0,0 +1,24 @@
+var headerTemplate = `
+<div>
+    <H1><a href="file:///Users/mattricks/voyager-archives/src/index.html">Voyager Archives</a></H1>
+</div>
+<div>
+    <!--
+    <div>
+        Log In | Create Account
+    </div>
+    <div>
+        Search
+    </div>
+    -->
+    <div>
+        <a href="file:///Users/mattricks/voyager-archives/src/DIY/Index.html">DIY</a>
+        <!-- <a href="file:///Users/mattricks/voyager-archives/src/Forums/Index.html">Forums</a> -->
+        <!-- <a href="file:///Users/mattricks/voyager-archives/src/KB/Index.html">KB</a> -->
+        <a href="file:///Users/mattricks/voyager-archives/src/Media/Index.html">Media</a>
+        <a href="file:///Users/mattricks/voyager-archives/src/Simulations/Index.html">Simulations</a>
+        <!-- <a href="file:///Users/mattricks/voyager-archives/src/Stories/Index.html">Stories</a> -->
+        <!-- <a href="file:///Users/mattricks/voyager-archives/src/Wiki/Index.html">Wiki</a> -->
+    </div>
+</div>
+`;
