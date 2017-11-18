8aed667d7f4095ab3fe25efceda03857b8362ebc
Matt Ricks <mgizmor37@gmail.com>
2017-11-18 10:18:41 -0700

    fixed the links... again.

--- Resources/header.js
+++ Resources/header.js
@@ -12,12 +12,12 @@ var headerTemplate = `
     </div>
     -->
     <div>
-        <a href="file:///Users/mattricks/voyager-archives/src/DIY/Index.html">DIY</a>
-        <!-- <a href="file:///Users/mattricks/voyager-archives/src/Forums/Index.html">Forums</a> -->
+        <a href="file:///Users/mattricks/voyager-archives/srcvoyager-archives/DIY/Index.html">DIY</a>
+        <!-- <a href="file:///Users/mattricks/voyager-archives/srcvoyager-archives/Forums/Index.html">Forums</a> -->
         <!-- <a href="file:///Users/mattricks/voyager-archives/src/KB/Index.html">KB</a> -->
         <a href="file:///Users/mattricks/voyager-archives/src/Media/Index.html">Media</a>
         <a href="file:///Users/mattricks/voyager-archives/src/Simulations/Index.html">Simulations</a>
-        <!-- <a href="file:///Users/mattricks/voyager-archives/src/Stories/Index.html">Stories</a> -->
+        <!-- <a href="file:///Users/mattricks/voyager-archives/srcvoyager-archives/Stories/Index.html">Stories</a> -->
         <!-- <a href="file:///Users/mattricks/voyager-archives/src/Wiki/Index.html">Wiki</a> -->
     </div>
 </div>
