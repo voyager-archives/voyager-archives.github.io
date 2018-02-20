109f3b47067ea6203b0d764e42d155e428d000d7
Matt Ricks <mgizmor37@gmail.com>
2018-02-20 08:32:49 -0700

    restructuring

---
+++ resources/template_engine/header.js
@@ -0,0 +1,25 @@
+var headerTemplate = `
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<link rel="stylesheet" href="/voyager-archives/Resources/header.css"></link>
+<div>
+    <a href="http://voyagerarchives.com">
+        <img src="./resources/images/logo.png" alt="Voyager Archives" height="120" width="80">
+    </a>
+    <h1>The Voyager Archives</h1>
+    <!-- <a style="float:right" href="">Log In</a> -->
+</div>
+<nav>
+    <ul>
+        <li>
+            <a href="/voyager-archives/DIY/index.html">DIY</a>
+        </li>
+        <!-- <li><a href="/voyager-archives/Forums/index.html">Forums</a></li> -->
+        <!-- <li><a href="/voyager-archives/KB/index.html">KB</a></li> -->
+        <li><a href="/voyager-archives/Media/index.html">Media</a></li>
+        <li><a href="/voyager-archives/Simulators/index.html">Simulations</a></li>
+        <!-- <li><a href="/voyager-archives/Stories/index.html">Stories</a></li> -->
+        <!-- <li><a href="/voyager-archives/Wiki/index.html">Wiki</a></li> -->
+        <!-- <li style="float:right"><a href="">Search</a></li> -->
+    </ul>
+</nav>
+`;
