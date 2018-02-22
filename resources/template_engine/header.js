5dbb845f32ef033674f215bf7acb1e4af7b64fb6
Matt Ricks <mgizmor37@gmail.com>
2018-02-22 15:11:33 -0700

    test

---
+++ resources/template_engine/header.js
@@ -0,0 +1,86 @@
+var headerTemplate = `
+<link rel="stylesheet" href="/Resources/header.css"></link>
+<a href="http://voyagerarchives.com/"><img class="header-image" src="http://voyagerarchives.com/resources/images/logo.png" height="100px"></a>
+<h1 class="header-title-h1">The Voyager Archives</h1>
+<ul>
+    <li class="dropdown">
+        <a href="http://voyagerarchives.com/News/index.html" class="dropbtn">News</a>
+        <div class="header-dropdown-content">
+            <a href="http://voyagerarchives.com/News/canyon-grove.html">Canyon Grove</a>
+            <a href="http://voyagerarchives.com/News/cmsec.html">CMSEC</a>
+            <a href="http://voyagerarchives.com/News/dreamflight.html">Dreamflight</a>
+            <a href="http://voyagerarchives.com/News/farpoint.html">Farpoint</a>
+            <a href="http://voyagerarchives.com/News/infinid.html">InfiniD</a>
+            <a href="http://voyagerarchives.com/News/news-articles.html">News Articles</a>
+            <a href="http://voyagerarchives.com/News/lions-gate.html">Lion's Gate</a>
+            <a href="http://voyagerarchives.com/News/drydock-talk.html">Drydock Talk</a>
+            <a href="http://voyagerarchives.com/News/telos.html">Telos</a>
+            <a href="http://voyagerslog.blogspot.com">Voyagers Log</a>
+        </div>
+    </li>
+    <li><a href="http://voyagerarchives.com/"><span>History</span></a></li>
+    <li><a href="http://voyagerarchives.com/"><span>Stories</span></a></li>
+    <li class="dropdown">
+        <a href="http://voyagerarchives.com/Simulators/index.html" class="dropbtn">Simulator</a>
+        <div class="header-dropdown-content">
+            <a href="">Training</a>
+            <a href="">Types</a>
+            <a href="">Builds</a>
+        </div>
+    </li>
+    <li class="dropdown">
+        <a href="http://voyagerarchives.com/DIY/index.html" class="dropbtn">Universe</a>
+        <div class="header-dropdown-content">
+            <a href="">United Federation of Planets</a>
+            <a href="">Farpoint</a>
+            <a href="">InfiniD</a>
+            <a href="">Colonial Command</a>
+        </div>
+    </li>
+    <li class="dropdown">
+        <a href="http://voyagerarchives.com/DIY/index.html" class="dropbtn">DIY</a>
+        <div class="header-dropdown-content">
+            <a href="">Bridge Design / Construction</a>
+            <a href="">Controls</a>
+            <a href="">Costumes</a>
+            <a href="">Graphic Design</a>
+            <a href="">Lighting</a>
+            <a href="">Music</a>
+            <a href="">Panels</a>
+            <a href="">Programming</a>
+            <a href="">Props</a>
+            <a href="">Running the Simulation</a>
+            <a href="">Sound Effects</a>
+            <a href="">Sound System</a>
+            <a href="">Tips and Tricks</a>
+            <a href="">Video System</a>
+            <a href="">Writing Missions</a>
+        </div>
+    </li>
+    <li class="dropdown">
+        <a href="http://voyagerarchives.com/Simulators/Media/index.html" class="dropbtn">Media</a>
+        <div class="header-dropdown-content">
+            <a href="">3D Models</a>
+            <a href="">Images</a>
+            <a href="">Missions</a>
+            <a href="">Music</a>
+            <a href="">Other</a>
+            <a href="">Pictures</a>
+            <a href="">Scripts</a>
+            <a href="">SFX</a>
+            <a href="">Software</a>
+            <a href="">Videos</a>
+        </div>
+    </li>
+    <li class="dropdown">
+        <a href="http://voyagerarchives.com/Simulators/volunteers.html" class="dropbtn">Get Involved</a>
+        <div class="header-dropdown-content">
+            <a href="">Build a Ship</a>
+            <a href="">Donate</a>
+            <a href="">Open Source Programming</a>
+            <a href="">Volunteering</a>
+        </div>
+    </li>
+    </li>
+</ul>
+`;
